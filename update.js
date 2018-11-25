009380acc6f436d3056fbef53dd05de7e22c5b58
Haifeng Mei <haifengmei@hotmail.com>
2018-11-25 11:15:07 -0400

    updated project names

--- update.js
+++ update.js
@@ -7,7 +7,7 @@ export async function main(event, context) {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be updated
     // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
+    // - 'subscriptionId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       subscriptionId: event.pathParameters.id
